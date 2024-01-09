9984dea87414bc9d926b33db40e182e80511cd99
boogie-bogie <86586908+boogie-bogie@users.noreply.github.com>
2024-01-09 09:44:14 +0900

    [level unrated] Title: 각도기, Time: 0.03 ms, Memory: 33.6 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/120829. 각도기/각도기.js
@@ -0,0 +1,8 @@
+function solution(angle) {
+    switch (angle) {
+        case 90: return 2; 
+        case 180: return 4;
+        default:
+            return angle > 0 && angle < 90 ? 1 : 3; 
+    }
+}
